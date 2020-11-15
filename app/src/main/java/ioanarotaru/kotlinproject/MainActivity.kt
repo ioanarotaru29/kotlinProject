d796b9539a2d4345952d0a25809faf842160d928
rotaruioana <riir2605@scs.ubbcluj.ro>
2020-11-15 19:52:29 +0200

    Add logout

--- app/src/main/java/ioanarotaru/kotlinproject/MainActivity.kt
+++ app/src/main/java/ioanarotaru/kotlinproject/MainActivity.kt
@@ -7,10 +7,16 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.FragmentNavigator
+import ioanarotaru.kotlinproject.auth.data.AuthRepository
 import ioanarotaru.kotlinproject.core.TAG
 
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +37,14 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_logout -> {
+                AuthRepository.logout()
+                finish();
+                overridePendingTransition( 0, 0);
+                startActivity(intent);
+                overridePendingTransition( 0, 0);
+                return true;
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
